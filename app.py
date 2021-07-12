828e72c81e3f2767061f261b27e5feab2bec6063
Sri-charan1069 <85877677+Sri-charan1069@users.noreply.github.com>
2021-07-13 04:44:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+test_model = joblib.load('sentiment_analyser')
+st.title('Sentiment Analysis of customer reviews')
+ip = st.text_input('Enter your review: ')
+op = test_model.predict([ip])
+if st.button('Sentiment Prediction'):
+  st.title(op[0])
